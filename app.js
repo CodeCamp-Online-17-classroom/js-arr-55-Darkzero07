de3d68a6e5d3ed66d0f68b12391b0e7de5909378
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-23 21:13:49 +0700

    js lab 141

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// write code here
+// จงหาและนับจำนวนตัวอักษรที่ปรากฏใน str
+
+let str = "I live in Thailand";
+
+const text = str.split(" ");
+
+const textCount = {};
+text.forEach((item) => {
+  textCount[item] = (textCount[item] || 0) + 1;
+});
+console.log(textCount);
+// expected result: {i: 4, l: 2, v: 1, e:1, n: 2, t: 1, h:1, a:2, d:1}
